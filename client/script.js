b5f40584d02e5835ee8d95841fbfd3b6d63d0ad8
Hi_Mrinal <mrinalbitsat@gmail.com>
2022-12-25 18:45:20 +0530

    Added : Done with the API

--- client/script.js
+++ client/script.js
@@ -5,24 +5,13 @@ const form = document.querySelector('form')
 const chatContainer = document.querySelector('#chat_container')
 
 
-function loader(element) {
-  element.textContent = ''
-
-  let loadInterval = setInterval(() => {
-    element.textContent += '.'
-
-    if(element.textContent === '....'){
-      element.textContent = ''
-    }   
-  }, 300)
-}
 
 function typeText(element, text){
   let index = 0
 
   let interval = setInterval(() => {
     if (index < text.length){
-      element.innerHTML += text.chartAt(index)
+      element.innerHTML += text.charAt(index)
       index++
     }
     else {
@@ -44,7 +33,7 @@ function chatStripe (isAi, value, uniqueId) {
     `
       <div class="wrapper ${isAi && 'ai'}">
         <div class="chat">
-          <div className="profile">
+          <div class="profile">
            <img
             src="${isAi ? bot : user}"
             alt="${isAi ? 'bot' : 'user'}"
@@ -72,7 +61,31 @@ const handleSubmit = async (e) => {
 
   const messageDiv = document.getElementById(uniqueId)
 
-  loader(messageDiv)
+  const response = await fetch('http://localhost:5000', {
+    methods: 'POST', 
+    headers: {
+      'Content-Type' : 'application/json'
+    },
+    body : JSON.stringify({
+      prompt : data.get('propmt')
+    })
+  })
+
+  clearInterval(loadInterval)
+  messageDiv.innerHTML = ' '
+
+  if(response.ok) {
+    const data = await response.json()
+    const parsedData = data.bot.trim()
+
+    console.log(parsedData)
+
+    typeText(messageDiv, parsedData)
+  } else {
+    const err = await response.text()
+
+    messageDiv.innerHTML = "Something Went Wrong"
+  }
 }
 
 form.addEventListener('submit', handleSubmit)
--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 })
 
+console.log(process.env.OPENAI_API_KEY)
 const openai = new OpenAIApi(configuration)
 
 const app = express()
