1986349a9ea2cddccd2c99fa0498cd3278b238f1
Hi_Mrinal <mrinalbitsat@gmail.com>
2022-12-25 16:49:19 +0530

    Added : Done with the API

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-import { express } from "express";
+import  express  from "express";
 import * as dotenv from "dotenv";
 import cors from "cors"; 
 import { Configuration, OpenAIApi } from "openai";
@@ -20,6 +20,31 @@ app.get('/', async(req, res) => {
         message: "Hello From the Mrinal"
     })
 })
+
+app.post('/', async (req, res) => {
+    try{
+        const prompt = req.body.prompt
+
+        const response = await openai.createCompletion({
+            model:"text-davinci-003",
+            prompt: `${prompt}`,
+            temperature:0,
+            max_tokens: 3000,
+            top_p:1,
+            frequency_penalty:0.5,
+            presence_penalty:0,
+        })
+
+        res.status(200).send({
+            bot : response.data.choices[0].text
+        })
+    }catch (error){
+        console.log(Error)
+        res.status(500).send({ error })
+    }
+})
+
+app.listen(5000, () => console.log('Server is running on port http://localhost:5000'))
 
 
 
