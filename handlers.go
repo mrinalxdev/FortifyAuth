ebb4dd0abe84b120ab65d882e979b88c1d4ced64
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-12-14 01:03:48 +0530

    Completed with the middleware and the handler

--- handlers.go
+++ handlers.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	// "fmt"
-	"net/http"
+	"fmt"
 	"html/template"
-	// "context"
-
-	// "github.com/gorilla/mux"
-	// "github.com/justinas/alice"
+	"net/http"
 )
 
 var templates *template.Template
@@ -30,5 +26,114 @@ func registerHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func registerSubmitHandler(w http.ResponseWriter, r *http.Request){
-	
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+
+	if !isValidUsername(username) || !isValidPassword(password) {
+		http.Error(w, "Invalid username or password", http.StatusBadRequest)
+		return
+	}
+
+	if _, exists := users[username]; exists{
+		http.Error(w, "Username already taken", http.StatusConflict)
+		return
+	}
+
+	roles := []string{"user"}
+
+	if preProcessFunc := getPreProcessFunc(username, password); preProcessFunc != nil {
+		preProcessFunc()
+	}
+
+	users[username] = User{Username: username, Password: password, Roles: roles}
+
+}
+
+func isValidUsername (username string) bool {
+	return len(username) >= 3
+}
+
+func isValidPassword(password string) bool {
+	return len(password) >= 6
+}
+
+func getPreProcessFunc(username, password string) func() {
+	return func() {
+		fmt.Printf("Pre-processing for user registration: %s\n", username)
+	}
+}
+
+func getPostRegistrationFunc(username string) func() {
+	return func() { 
+		fmt.Printf("Post-registration logic for user: %s\n", username) 
+	}
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request){
+	renderTemplate(w, "login.html", nil)
+}
+
+func loginSubmitHandler(w http.ResponseWriter, r *http.Request){
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+
+	user, found := users[username]
+	if !found || user.Password != password {
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
+
+	session, _ := store.Get(r, "session-name")
+	session.Values["username"] = username
+	session.Save(r, w)
+
+	http.Redirect(w, r, "/user", http.StatusSeeOther)
+}
+
+func logoutHandler(w http.ResponseWriter, r *http.Request){
+	session, _ := store.Get(r, "session-time")
+
+	username := getUsernameFromContext(r.Context())
+	if preLogoutFunc := getPreLogoutFunc(username); preLogoutFunc != nil {
+		preLogoutFunc()
+	}
+
+	delete(session.Values, "username")
+	session.Save(r, w)
+
+	if postLogoutFunc := getPostLogoutFunc(username); postLogoutFunc != nil {
+		postLogoutFunc()
+	}
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+
+func getPostLogoutFunc(username string) func() {
+	if username == "admin" {
+		return func() {
+			fmt.Printf("Executing admin post-logout logic")
+		}
+	}
+
+	return nil
+}
+
+
+func getPreLogoutFunc(username string) func() {
+	if username == "admin" {
+		return func() {
+			fmt.Printf("Executing admin post-logout logic")
+		}
+	}
+
+	return nil
+}
+
+func adminHandler(w http.ResponseWriter, r *http.Request){
+	renderTemplate(w, "admin.html", nil)
+}
+
+func userhandler(w http.ResponseWriter, r *http.Request){
+	renderTemplate(w, "user.html", nil)
 }
--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/justinas/alice"
-	// "github.com/justinas/alice"
 	"github.com/gorilla/sessions"
 )
 
@@ -15,10 +14,23 @@ func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/register", registerHandler).Methods("GET")
+	r.HandleFunc("/register", registerSubmitHandler).Methods("POST")
+	r.HandleFunc("/login", loginHandler).Methods("GET")
+	r.HandleFunc("/login", loginSubmitHandler).Methods("POST")
+	r.HandleFunc("/logut", logoutHandler).Methods("GET")
 
 
 	// r.Handle("/admin", alice.New(usernameMiddleware, roleMiddleware("admin")).ThenFunc(adminHandler))
+	r.Handle("/admin", alice.New(usernameMiddleware, roleMiddleware("admin")).ThenFunc(adminHandler))
+	r.Handle("/user", alice.New(usernameMiddleware, roleMiddleware("user")).ThenFunc(userhandler))
 
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static", http.FileServer(http.Dir("static"))))
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		renderTemplate(w, "index.html", nil)
+	}).Methods("GET")
+
+	http.Handle("/", r)
 
 	fmt.Println("Server listening on :8080")
 	http.ListenAndServe(":8080", nil)
